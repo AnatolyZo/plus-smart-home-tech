52ef4ab60ea7f30c4e6b8d8ee15d21ef80d9839d
Anatoly <tolikto@yandex.ru>
2026-08-14 21:02:21 +0300

    Привет! Выполнено задание спринта 19

---
+++ telemetry/collector/src/main/java/ru/practicum/telemetry/exception/ErrorHandler.java
@@ -0,0 +1,18 @@
+package ru.practicum.telemetry.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleUnexpectedError(final RuntimeException e) {
+        return Map.of("Непредвиденная ошибка", e.getMessage());
+    }
+}
---
+++ telemetry/collector/src/main/java/ru/practicum/telemetry/hubs/HubEventType.java
@@ -0,0 +1,8 @@
+package ru.practicum.telemetry.hubs;
+
+public enum HubEventType {
+    DEVICE_ADDED,
+    DEVICE_REMOVED,
+    SCENARIO_ADDED,
+    SCENARIO_REMOVED
+}
